2cdbdd665b7a3aa767aed5520000f16975127e21
Adam Rasheed <adamrasheed91@gmail.com>
2017-09-05 17:42:23 -0700

    added json stuff

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,50 @@
-const msg = 'ES6 is now ES5!';
+const api = "https://api.myjson.com/bins/x91hl";
 
-console.log(msg);
+const body = document.body;
+const postContainer = document.querySelector('#site-posts');
+
+fetch(api)
+    .then(blob => blob.json())
+    .then(data => {
+    
+    const posts = data;
+    
+    // create createNode
+    // createNode
+    function createNode(el){
+        return document.createElement(el);
+    }
+    
+    // for loop
+    for( const post of posts){
+//        console.log(post.Title);
+        
+    const postDiv = createNode('div');
+    postDiv.className = 'post';
+    postDiv.innerHTML =
+        `<p class="post__category post__category--${post.Category.toLowerCase()} ">${post.Category}</p>
+        <h2 class="post__title">${post.Title}</h2>
+
+        <div class="post__meta post__meta--${post.Category.toLowerCase()}">
+            <div class="post__comment">
+                <i class="fa fa-comment" aria-hidden="true"></i> ${post.Comments} ${post.Comments != 1 ? ` Comments` : ` Comment`} ${post.Comments > 5 ? ` <i class="fa fa-fire" aria-hidden="true"></i>` : ``}
+            </div>
+
+            <div class="post__time">
+                <i class="fa fa-clock-o" aria-hidden="true"></i>
+                <span>${post.Date}</span>
+            </div>
+        </div>`;
+        
+    // insert posts into post container
+    postContainer.appendChild(postDiv);
+        
+    } // end of for loop
+    
+    
+    
+    
+    // slice to 8 posts
+    
+    })
+    .catch(error => console.log(error));
